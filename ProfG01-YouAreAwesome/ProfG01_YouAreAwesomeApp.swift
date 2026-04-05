2cb413df70abc4e295a5ea24c7bec0c56950b65a
Daniel Martin Machadinho <daniel.martin.bsas@gmail.com>
2026-04-05 19:52:51 +0200

    Initial Commit

---
+++ ProfG01-YouAreAwesome/ProfG01_YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfG01_YouAreAwesomeApp.swift
+//  ProfG01-YouAreAwesome
+//
+//  Created by Daniel Martin on 05/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ProfG01_YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
