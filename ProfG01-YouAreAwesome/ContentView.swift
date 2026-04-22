b168a2a093ae36184fa3df05b0e5b014bb51ad03
Daniel Martin Machadinho <daniel.martin.bsas@gmail.com>
2026-04-22 15:29:27 +0200

    Random UI

--- ProfG01-YouAreAwesome/ContentView.swift
+++ ProfG01-YouAreAwesome/ContentView.swift
@@ -1,19 +1,58 @@
-//
-//  ContentView.swift
-//  ProfG01-YouAreAwesome
-//
-//  Created by Daniel Martin on 05/04/2026.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message: String = ""
+    @State private var imageName: String = ""
+    @State private var lastMessageNumber: Int = -1
+    @State private var lastImageNumber: Int = -1
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+                .shadow(radius: 30)
+                .animation(.default, value: imageName)
+                        
+            Spacer()
+            
+            Button("Click me!") {
+                let messages = [
+                    "You are Great",
+                    "You are Awesome",
+                    "You are Perfect",
+                    "You are Amazing",
+                    "You are the Coolest"
+                ]
+                
+                //Random text
+                var messageNumber: Int
+                repeat {
+                    messageNumber = Int.random(in: 0...messages.count-1)
+                } while messageNumber == lastMessageNumber
+                message = messages[messageNumber]
+                lastMessageNumber = messageNumber
+                
+                //Random image
+                var imageNumber: Int
+                repeat {
+                    imageNumber = Int.random(in: 0...9)
+                } while imageNumber == lastImageNumber
+                imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
     }
